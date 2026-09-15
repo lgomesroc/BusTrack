0ee41c10cfd98ce16ca5c96e36e52517bee84bde
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-15 15:49:01 -0300

    Ajusta teste de atualizacao de senha

--- BusTrack.Frontend/bustrack.frontend.client/src/app/login/update-password/update-password.component.spec.ts
+++ BusTrack.Frontend/bustrack.frontend.client/src/app/login/update-password/update-password.component.spec.ts
@@ -1,17 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
 import { UpdatePasswordComponent } from './update-password.component';
 
-describe('UpdatePasswordComponentComponent', () => {
+describe('UpdatePasswordComponent', () => {
   let component: UpdatePasswordComponent;
   let fixture: ComponentFixture<UpdatePasswordComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UpdatePasswordComponent]
+      declarations: [UpdatePasswordComponent],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule
+      ]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(UpdatePasswordComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
