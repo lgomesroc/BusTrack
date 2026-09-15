156433cc7c1bfafec59a9fbc0348f735962cd56a
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-15 15:48:59 -0300

    Ajusta interface de autenticacao

--- BusTrack.API/InterfacesAPI/IServicesAPI/IUserAuthenticationServiceAPI.cs
+++ BusTrack.API/InterfacesAPI/IServicesAPI/IUserAuthenticationServiceAPI.cs
@@ -5,10 +5,14 @@ namespace BusTrack.BusTrack.API.InterfacesAPI.IServicesAPI
 {
     public interface IUserAuthenticationServiceAPI
     {
-        UserDB Authenticate(string email, string password);
+        UserDB? Authenticate(string email, string password);
+
         IActionResult Create(UserDB user);
-        UserDB Read(string id);
+
+        UserDB? Read(string id);
+
         IActionResult Update(string id, UserDB user);
+
         IActionResult Delete(string id);
     }
 }
