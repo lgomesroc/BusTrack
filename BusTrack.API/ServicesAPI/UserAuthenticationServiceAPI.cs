46e267d588ef5ae013de541ca05c96408768291a
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-15 15:48:59 -0300

    Adiciona validacao de senha na autenticacao

--- BusTrack.API/ServicesAPI/UserAuthenticationServiceAPI.cs
+++ BusTrack.API/ServicesAPI/UserAuthenticationServiceAPI.cs
@@ -1,8 +1,8 @@
 ﻿using BusTrack.BusTrack.API.InterfacesAPI.IServicesAPI;
 using BusTrack.BusTrack.DB.ClassesDB;
 using BusTrack.BusTrack.DB.InterfacesDB.IRepositoriesDB;
-using BusTrack.BusTrack.DB.RepositoriesDB;
 using Microsoft.AspNetCore.Mvc;
+using BCryptNet = BCrypt.Net.BCrypt;
 
 namespace BusTrack.BusTrack.API.ServicesAPI
 {
@@ -10,60 +10,55 @@ namespace BusTrack.BusTrack.API.ServicesAPI
     {
         private readonly IUserRepositoryDB _userRepository;
 
-        public UserAuthenticationServiceAPI(IUserRepositoryDB userRepository)
+        public UserAuthenticationServiceAPI(
+            IUserRepositoryDB userRepository)
         {
             _userRepository = userRepository;
         }
 
-        public UserDB Authenticate(string email, string password)
+        public UserDB? Authenticate(string email, string password)
         {
             var user = _userRepository.GetByEmail(email);
 
-            if (user != null && user.Password == password)
-            {
-                return user;
-            }
-            else
+            if (user == null || string.IsNullOrWhiteSpace(user.Password))
             {
                 return null;
             }
+
+            if (BCryptNet.Verify(password, user.Password))
+            {
+                return user;
+            }
+
+            return null;
         }
 
         public IActionResult Create(UserDB user)
         {
             _userRepository.Create(user);
+
             return new OkResult();
         }
 
-        public UserDB Read(string id)
-        {
-            return _userRepository.Read(id);
-        }
+        public UserDB? Read(string id) =>
+            _userRepository.Read(id);
 
         public IActionResult Update(string id, UserDB user)
         {
             var result = _userRepository.Update(id, user);
-            if (result)
-            {
-                return new OkResult();
-            }
-            else
-            {
-                return new NotFoundResult();
-            }
+
+            return result
+                ? new OkResult()
+                : new NotFoundResult();
         }
 
         public IActionResult Delete(string id)
         {
             var result = _userRepository.Delete(id);
-            if (result)
-            {
-                return new OkResult();
-            }
-            else
-            {
-                return new NotFoundResult();
-            }
+
+            return result
+                ? new OkResult()
+                : new NotFoundResult();
         }
     }
 }
