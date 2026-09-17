8d70daff43ae76e1b9761ec32eb21a654b4d38d0
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:33:24 -0300

    Atualiza modelo de viagens do DB

--- BusTrack.DB/ModelsDB/TripModelDB.cs
+++ BusTrack.DB/ModelsDB/TripModelDB.cs
@@ -5,12 +5,19 @@ namespace BusTrack.BusTrack.DB.ModelsDB
     public class TripModelDB : ITripModelDB
     {
         public int Id { get; set; }
+
         public int BusId { get; set; }
+
         public DateTime DepartureTime { get; set; }
+
         public DateTime ArrivalTime { get; set; }
+
         public int Duration { get; set; }
+
         public int LimitPassengers { get; set; }
-        public List<int> Passengers { get; set; }
+
+        public List<int> Passengers { get; set; } = new();
+
         public int DriverId { get; set; }
     }
 }
