01f8bd08aece35dea96a7c23c426dc63fe006c68
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:32:11 -0300

    Corrige perfil de mapeamento da API

--- BusTrack.API/MappingsAPI/MappingProfileAPI.cs
+++ BusTrack.API/MappingsAPI/MappingProfileAPI.cs
@@ -1,19 +1,22 @@
 ﻿using AutoMapper;
 using BusTrack.BusTrack.API.DTOAPI;
 using BusTrack.BusTrack.API.ModelsAPI;
+using BusTrack.BusTrack.DB.Classes;
 
 namespace BusTrack.BusTrack.API.MappingsAPI
 {
     public class MappingProfileAPI : Profile
     {
-        public void MappingProfile()
+        public MappingProfileAPI()
         {
-            CreateMap<BusModelAPI, BusDTOAPI>();
-            CreateMap<RouteModelAPI, RouteDTOAPI>();
+            CreateMap<TripDB, TripDTOAPI>();
+            CreateMap<TripDTOAPI, TripDB>();
+
+            CreateMap<TripDTOAPI, TripModelAPI>();
             CreateMap<TripModelAPI, TripDTOAPI>();
-            CreateMap<PassengerModelAPI, PassengerDTOAPI>();
-            CreateMap<DriverModelAPI, DriverDTOAPI>();
-            CreateMap<TripsPassengerModelAPI, TripPassengerDTOAPI>();
+
+            CreateMap<TripPassengerDB, TripPassengerDTOAPI>();
+            CreateMap<TripPassengerDTOAPI, TripPassengerDB>();
         }
     }
 }
