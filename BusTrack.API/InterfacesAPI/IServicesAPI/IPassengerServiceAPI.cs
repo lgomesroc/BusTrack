9dbcd4a91b76b5b71c7b4e3bb143975786caa981
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:31:51 -0300

    Atualiza contrato do servico de passageiros

--- BusTrack.API/InterfacesAPI/IServicesAPI/IPassengerServiceAPI.cs
+++ BusTrack.API/InterfacesAPI/IServicesAPI/IPassengerServiceAPI.cs
@@ -5,16 +5,43 @@ namespace BusTrack.BusTrack.API.InterfacesAPI.IServicesAPI
 {
     public interface IPassengerServiceAPI
     {
-        Task<IEnumerable<PassengerDTOAPI>> GetAllPassengers();
-        Task<PassengerDTOAPI> GetPassengerById(string id);
-        Task<PassengerDTOAPI> CreatePassenger(PassengerDTOAPI passenger);
-        Task<PassengerDTOAPI> UpdatePassenger(string id, PassengerDTOAPI passenger);
-        Task<bool> DeletePassenger(string id);
-        Task<List<PassengerDB>> GetPassengers();
-        Task AddPassengerAsync(PassengerDTOAPI passengerDto);
-        Task<PassengerDTOAPI> GetPassengerByIdAsync(int id);
-        Task UpdatePassengerAsync(PassengerDTOAPI passengerDto);
-        Task DeletePassengerAsync(int id);
-        Task<IEnumerable<PassengerDTOAPI>> GetAllPassengersAsync();
+        Task<IEnumerable<PassengerDTOAPI>>
+            GetAllPassengers();
+
+        Task<PassengerDTOAPI?>
+            GetPassengerById(
+                string id);
+
+        Task<PassengerDTOAPI>
+            CreatePassenger(
+                PassengerDTOAPI passenger);
+
+        Task<PassengerDTOAPI>
+            UpdatePassenger(
+                string id,
+                PassengerDTOAPI passenger);
+
+        Task<bool>
+            DeletePassenger(
+                string id);
+
+        Task<List<PassengerDB>>
+            GetPassengers();
+
+        Task AddPassengerAsync(
+            PassengerDTOAPI passengerDto);
+
+        Task<PassengerDTOAPI>
+            GetPassengerByIdAsync(
+                int id);
+
+        Task UpdatePassengerAsync(
+            PassengerDTOAPI passengerDto);
+
+        Task DeletePassengerAsync(
+            int id);
+
+        Task<IEnumerable<PassengerDTOAPI>>
+            GetAllPassengersAsync();
     }
 }
