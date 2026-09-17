09eb731cc4e1dda3eb2e3adfe3b2b6e4630db9e3
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:34:47 -0300

    Atualiza configuracao da aplicacao

--- BusTrack.Program/Program.cs
+++ BusTrack.Program/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using BusTrack.BusTrack.API.InterfacesAPI.IServicesAPI;
 using BusTrack.BusTrack.API.ServicesAPI;
 using BusTrack.BusTrack.DB.InterfacesDB.IRepositoriesDB;
@@ -11,58 +12,106 @@ public partial class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        builder.Configuration.AddJsonFile("appsettings.json");
+        builder.Configuration.AddJsonFile(
+            "appsettings.json");
 
         builder.Services.AddControllers();
+
         builder.Services.AddEndpointsApiExplorer();
+
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddAutoMapper(
+            (serviceProvider, mapperConfiguration) =>
+            {
+                mapperConfiguration.AddProfile<
+                    BusTrack.BusTrack.API.MappingsAPI.MappingProfileAPI>();
+            },
+            Array.Empty<Assembly>());
 
         builder.Services.AddCors(options =>
         {
-            options.AddPolicy("FrontendPolicy", policy =>
-            {
-                policy
-                    .WithOrigins("https://127.0.0.1:4200")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod();
-            });
+            options.AddPolicy(
+                "FrontendPolicy",
+                policy =>
+                {
+                    policy
+                        .WithOrigins(
+                            "https://127.0.0.1:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
         });
 
-        builder.Services.AddDatabaseServices(builder.Configuration);
+        builder.Services.AddDatabaseServices(
+            builder.Configuration);
 
         var connectionString =
-            builder.Configuration.GetConnectionString("BusTrackDBConnection");
+            builder.Configuration.GetConnectionString(
+                "BusTrackDBConnection");
 
         var databaseName =
-            builder.Configuration.GetConnectionString("DatabaseName")
-            ?? builder.Configuration["ConnectionStrings:DatabaseName"];
+            builder.Configuration.GetConnectionString(
+                "DatabaseName")
+            ?? builder.Configuration[
+                "ConnectionStrings:DatabaseName"];
 
-        builder.Services.AddSingleton<IMongoDatabase>(sp =>
-        {
-            var client = sp.GetRequiredService<IMongoClient>();
-            return client.GetDatabase(databaseName);
-        });
+        builder.Services.AddSingleton<IMongoDatabase>(
+            serviceProvider =>
+            {
+                var client =
+                    serviceProvider
+                        .GetRequiredService<IMongoClient>();
 
-        builder.Services.AddScoped<IUserRepositoryDB, UserRepositoryDB>();
+                return client.GetDatabase(
+                    databaseName);
+            });
+
+        builder.Services.AddScoped<
+            IUserRepositoryDB,
+            UserRepositoryDB>();
 
         builder.Services.AddScoped<
             IUserAuthenticationServiceAPI,
             UserAuthenticationServiceAPI>();
 
-        builder.Services.AddScoped<IAccountServiceAPI>(_ =>
-            new AccountServiceAPI(connectionString!, databaseName!));
+        builder.Services.AddScoped<
+            IAccountServiceAPI>(
+            serviceProvider =>
+                new AccountServiceAPI(
+                    connectionString!,
+                    databaseName!));
+
+        builder.Services.AddScoped<
+            ITripRepositoryDB,
+            TripRepositoryDB>();
+
+        builder.Services.AddScoped<
+            ITripServiceAPI,
+            TripServiceAPI>();
+
+        builder.Services.AddScoped<
+            ITripPassengerRepositoryDB,
+            TripPassengerRepositoryDB>();
+
+        builder.Services.AddScoped<
+            ITripsPassengerServiceAPI,
+            TripsPassengerServiceAPI>();
 
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
+
             app.UseSwaggerUI();
+
             app.UseDeveloperExceptionPage();
         }
         else
         {
             app.UseExceptionHandler("/Error");
+
             app.UseHsts();
         }
 
@@ -76,7 +125,8 @@ public partial class Program
 
         app.MapControllerRoute(
             name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
+            pattern:
+                "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
