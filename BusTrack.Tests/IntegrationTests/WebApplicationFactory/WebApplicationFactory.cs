48daff23946cc8f23f01e8604ccf789fed981aa6
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:35:04 -0300

    Atualiza infraestrutura da factory de testes

--- BusTrack.Tests/IntegrationTests/WebApplicationFactory/WebApplicationFactory.cs
+++ BusTrack.Tests/IntegrationTests/WebApplicationFactory/WebApplicationFactory.cs
@@ -1,22 +1,31 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
 
 namespace BusTrack.Tests.IntegrationTests.CustomWebApplicationFactory
 {
-    public class WebApplicationFactory<TProgram> where TProgram : class
+    public class WebApplicationFactory<TProgram>
+        where TProgram : class
     {
-        protected virtual void ConfigureWebHost(IWebHostBuilder builder)
+        protected virtual void ConfigureWebHost(
+            IWebHostBuilder builder)
         {
             builder.UseStartup<TProgram>();
         }
 
         public HttpClient CreateClient()
         {
-            var builder = new WebHostBuilder();
-            ConfigureWebHost(builder);
-            var server = new TestServer(builder);
-            return server.CreateClient();
+            var host =
+                new HostBuilder()
+                    .ConfigureWebHostDefaults(
+                        builder =>
+                        {
+                            ConfigureWebHost(builder);
+                            builder.UseTestServer();
+                        })
+                    .Start();
+
+            return host.GetTestClient();
         }
     }
 }
-
