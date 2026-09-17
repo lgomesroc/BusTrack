2b79dfaee0011f59487a87cfb4e452225ba284d6
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:31:31 -0300

    Atualiza DTO de viagens

--- BusTrack.API/DTOAPI/TripDTOAPI.cs
+++ BusTrack.API/DTOAPI/TripDTOAPI.cs
@@ -3,9 +3,21 @@
     public class TripDTOAPI
     {
         public string? Id { get; set; }
+
         public string? BusId { get; set; }
+
         public string? DriverId { get; set; }
+
         public string? RouteId { get; set; }
+
         public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+
+        public int Duration { get; set; }
+
+        public int LimitPassengers { get; set; }
+
+        public List<string>? Passengers { get; set; }
     }
 }
