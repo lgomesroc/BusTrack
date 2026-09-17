2d080561be981a0b0ed9fddf57779cd121b37685
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:32:55 -0300

    Inicializa historico de senhas

--- BusTrack.DB/ClassesDB/UserPasswordHistoryDB.cs
+++ BusTrack.DB/ClassesDB/UserPasswordHistoryDB.cs
@@ -2,8 +2,10 @@ namespace BusTrack.BusTrack.DB.ClassesDB
 {
     public class UserPasswordHistoryDB
     {
-       public int Id { get; set; }
-       public string? Email { get; set; }
-       public List<PasswordHistoryDB> PasswordHistories { get; set; }
+        public int Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public List<PasswordHistoryDB> PasswordHistories { get; set; } = new();
     }
 }
