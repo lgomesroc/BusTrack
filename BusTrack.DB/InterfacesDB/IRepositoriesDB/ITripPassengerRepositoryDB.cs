74d79171d12a23b46d58a509db82d58bc8250da3
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:33:12 -0300

    Atualiza contrato do repositorio de passageiros das viagens

--- BusTrack.DB/InterfacesDB/IRepositoriesDB/ITripPassengerRepositoryDB.cs
+++ BusTrack.DB/InterfacesDB/IRepositoriesDB/ITripPassengerRepositoryDB.cs
@@ -1,19 +1,27 @@
 ﻿using BusTrack.BusTrack.DB.Classes;
-using BusTrack.BusTrack.DB.ModelsDB;
 
 namespace BusTrack.BusTrack.DB.InterfacesDB.IRepositoriesDB
 {
     public interface ITripPassengerRepositoryDB
     {
-        Task<IEnumerable<TripPassengerDB>> GetAllTripPassengersAsync();
-        Task<IEnumerable<int>> GetPassengerIdsByTripIdAsync(int tripId);
-        Task<IEnumerable<int>> GetTripIdsByPassengerIdAsync(int passengerId);
-        Task AddTripPassengerAsync(TripPassengerDB tripPassenger);
-        Task RemoveTripPassengerAsync(int tripId, int passengerId);
-        Task<IEnumerable<TripPassengerModelDB>> GetAllTripsPassengers();
-        Task<TripPassengerModelDB> GetTripsPassengerById(int id);
-        Task<TripPassengerDB> CreateTripsPassenger(TripPassengerDB tripsPassenger);
-        Task<TripPassengerDB> UpdateTripsPassenger(int id, TripPassengerDB tripPassenger);
-        Task<bool> DeleteTripsPassenger(int id);
+        Task<IEnumerable<TripPassengerDB>> GetAllAsync();
+
+        Task<TripPassengerDB?> GetByIdAsync(string id);
+
+        Task<IEnumerable<TripPassengerDB>> GetByTripIdAsync(string tripId);
+
+        Task<IEnumerable<TripPassengerDB>> GetByPassengerIdAsync(string passengerId);
+
+        Task<TripPassengerDB> CreateAsync(TripPassengerDB tripPassenger);
+
+        Task<TripPassengerDB?> UpdateAsync(
+            string id,
+            TripPassengerDB tripPassenger);
+
+        Task<bool> DeleteAsync(string id);
+
+        Task<bool> DeleteByTripAndPassengerAsync(
+            string tripId,
+            string passengerId);
     }
 }
