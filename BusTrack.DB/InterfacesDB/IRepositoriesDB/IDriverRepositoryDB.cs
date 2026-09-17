c6574711005825c14f8f6a235a79ad17c315a727
Luciano Rocha <lgomesroc2012@gmail.com>
2026-09-17 13:33:09 -0300

    Corrige nulabilidade do repositorio de motoristas

--- BusTrack.DB/InterfacesDB/IRepositoriesDB/IDriverRepositoryDB.cs
+++ BusTrack.DB/InterfacesDB/IRepositoriesDB/IDriverRepositoryDB.cs
@@ -5,7 +5,7 @@ namespace BusTrack.BusTrack.DB.InterfacesDB.IRepositoriesDB
     public interface IDriverRepositoryDB
     {
         Task<IEnumerable<DriverDB>> GetAllDriversAsync();
-        Task<DriverDB> GetDriverByIdAsync(string id);
+        Task<DriverDB?> GetDriverByIdAsync(string id);
         Task AddDriverAsync(DriverDB driver);
         Task UpdateDriverAsync(string id, DriverDB driver);
         Task DeleteDriverAsync(string id);
